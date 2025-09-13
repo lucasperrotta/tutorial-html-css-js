2e53a6a7687972877fece5fcbaf088e71269a3f0
Lucas Perrotta Barbosa <perrotta.lucas@gmail.com>
2025-09-13 15:19:34 -0300

    show buttons card stock

--- iffinance/js/main.js
+++ iffinance/js/main.js
@@ -26,7 +26,7 @@ function addTicker(event) {
   const total = valor * ativos;
 
   const card = `
-    <div class="card">
+    <div class="card" onmouseenter="showButtons(event)" onmouseleave="hideButtons(event)">
       <header>
         <h2>${ticker}</h2>
         <h3>${bolsa}</h3>
@@ -40,6 +40,10 @@ function addTicker(event) {
         <p>Nº Ativos: <span class="bold">${ativos}</span></p>
         <p>R$ ${total.toFixed(2)}</p>
       </footer>
+      <div class="buttons">
+        <button type="button" onclick="">Editar</button>
+        <button type="button" onclick="">Excluir</button>
+      </div>  
     </div>
   `;
 
@@ -48,4 +52,15 @@ function addTicker(event) {
   closeModal();
   // document.getElementById("cards").insertAdjacentHTML("beforeend", card); Caso fosse id não classe.
   // cardsContainer.insertAdjacentHTML("beforeend", card);
+}
+function showButtons(event) {
+  const cardStock = event.target;
+  const buttons = cardStock.querySelector(".buttons");
+  buttons.style.display = "flex";
+}
+
+function hideButtons(event) {
+  const cardStock = event.target;
+  const buttons = cardStock.querySelector(".buttons");
+  buttons.style.display = "none";
 }
